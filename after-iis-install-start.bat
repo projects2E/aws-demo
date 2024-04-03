154cdecdc4c4ecbf010cad5fc59b596bcda26c82
Eficiente & Eficaz <158305503+projects2E@users.noreply.github.com>
2024-04-03 18:28:16 -0500

    Update after-iis-install-start.bat

--- after-iis-install-start.bat
+++ after-iis-install-start.bat
@@ -1,6 +1,11 @@
 @echo off
 REM - File: after-iis-install-start.bat 
 
+xcopy /Y C:\temp\cliente1-web.config C:\inetpub\wwwroot\cliente1\api\web.config
+xcopy /Y C:\temp\cliente2-web.config C:\inetpub\wwwroot\cliente2\api\web.config
+xcopy /Y C:\temp\cliente3-web.config C:\inetpub\wwwroot\cliente3\api\web.config
+
+
 echo Restarting IIS...
 echo ======================================================
 
